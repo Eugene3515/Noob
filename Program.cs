1431dd2860a2a6a1d5ac7357404793bbb57b5cc9
Eugene1315 <hibin.eugene@yandex.ru>
2023-10-12 11:27:32 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static double Function(double x)
+    {
+        return x * Math.Cos(x) - 1;
+    }
+
+    static double BisectionMethod(double a, double b, double epsilon)
+    {
+        double c;
+
+        while ((b - a) >= epsilon)
+        {
+            c = (a + b) / 2;
+
+            if (Function(c) == 0.0)
+                break;
+
+            if (Function(c) * Function(a) < 0)
+                b = c;
+            else
+                a = c;
+        }
+
+        return (a + b) / 2;
+    }
+
+    static void Main(string[] args)
+    {
+        double a, b, epsilon;
+
+        Console.WriteLine("Введите границы интервала [a, b]:");
+        a = double.Parse(Console.ReadLine());
+        b = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите требуемую точность (epsilon):");
+        epsilon = double.Parse(Console.ReadLine());
+
+        double root = BisectionMethod(a, b, epsilon);
+
+        Console.WriteLine("Корень уравнения: " + root);
+    }
+}
